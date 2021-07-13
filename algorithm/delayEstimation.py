735b98c0c845ec4e449f05556c2d6139033dafd5
Marcin Bosk <marcin@MacBook-Pro.localdomain>
2021-07-13 20:19:48 +0200

    Repository cleanup for inclusion in paper

--- algorithm/delayEstimation.py
+++ algorithm/delayEstimation.py
@@ -4,13 +4,11 @@ sys.path.insert(0,currentdir)
 
 import numpy as np
 
-cliTypeFuncMapping = {'hostSSH' : [900, 0.288, 1],#[8.99e+02, 2.82e-01, 9.43e+00],
-                      'hostVIP' : [2.66e+05, 2.80e-01, 2.64e+01],#[30, 0.00435, 0],#[1.52e+03, 4.35e-03, 2],
-                    #   'hostVID' : [1600,0.0013,10],
+# These functions were determined experimentally using the LS tests from "baselineTestV3.ini"
+cliTypeFuncMapping = {'hostSSH' : [900, 0.288, 1],
+                      'hostVIP' : [2.66e+05, 2.80e-01, 2.64e+01],
                       'hostVID' : [1.67e+02, 3.54e-03, 1.53e-22],
-                    #   'hostLVD' : [500, 0.0011, 60],#[40, 0.00568, 1], 
                       'hostLVD' : [1.69e+01, 4.83e-03, 5.50e-01],
-                    #   'hostFDO' : [600, 0.0006, 20]#[450,0.0006,20]
                       'hostFDO' : [4.51e+01, 4.21e-03, 1.66e+00]
                      }
 
--- analysis/code/videoMOScalcFiles/code/ITUp1203/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "1.2.7"
-
-from .p1203Pa import P1203Pa
-from .p1203Pv import P1203Pv
-from .p1203Pq import P1203Pq
-from .itu_p1203 import P1203Standalone
